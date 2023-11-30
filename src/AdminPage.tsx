99526741d117e306114ebb0c7eb09d07d6014767
Hunternif <hunternif@gmail.com>
2023-11-30 23:44:12 +0000

    Add admin form template to upload new deck

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -1,6 +1,8 @@
 import { GoogleAuthProvider, User, signInWithPopup } from "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { firebaseAuth, useFetchCAAUser } from "./firebase";
+import Form from "react-bootstrap/Form";
+import { Button, Container } from "react-bootstrap";
 
 function LogInBox() {
   const signInWithGoogle = () => {
@@ -17,17 +19,36 @@ interface UserProps {
 function LoggedInView({ user }: UserProps) {
   const [caaUser, loading] = useFetchCAAUser(user.email ?? "invalid");
   const isAdmin = caaUser?.is_admin ?? false;
-  return <div className='admin-container'>
-    <div className='admin-header'>
-      <span>Hello, {user.displayName}!</span>
-      <button onClick={() => firebaseAuth.signOut()}>Sign out</button>
-    </div>
-    { !loading && (isAdmin ? <AdminContent/> : <div>Access denied</div>)}
-  </div>;
+  return (
+    <Container>
+      <div className='admin-header'>
+        <span>Hello, {user.displayName}!</span>
+        <button onClick={() => firebaseAuth.signOut()}>Sign out</button>
+      </div>
+      {!loading && (isAdmin ? <AdminContent /> : <div>Access denied</div>)}
+    </Container>
+  );
 }
 
 function AdminContent() {
-  return <div>Access granted</div>;
+  return <>
+    <h2>Upload new deck</h2>
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text" name="title" />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Questions</Form.Label>
+        <Form.Control as="textarea" name="questions" rows={10} />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Answers</Form.Label>
+        <Form.Control as="textarea" name="answers" rows={10} />
+      </Form.Group>
+      <Button type="button">Submit</Button>
+    </Form>
+  </>;
 }
 
 function AdminPage() {
