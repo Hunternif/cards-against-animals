e48f9b342dc99acc12ded103cda82a1671e6afb4
Hunternif <hunternif@gmail.com>
2023-12-04 23:35:00 +0000

    Call findOrCreateLobby only once after login

--- client/src/components/Lobby.tsx
+++ client/src/components/Lobby.tsx
@@ -1,24 +1,12 @@
 import { User } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { useEffectOnce } from "./utils";
 import { GameLobby } from "../model/types";
-import { findOrCreateLobby } from "../model/lobby-api";
 
 interface Props {
   user: User;
+  lobby: GameLobby;
 }
 
-export function Lobby({ user }: Props) {
-  const [error, setError] = useState<Error | null>(null);
-
-  const [lobby, setLobby] = useState<GameLobby | null>(null);
-
-  useEffectOnce(() => {
-    findOrCreateLobby(user).then(
-      (newLobby) => setLobby(newLobby)
-    ).catch(e => setError(e));
-  });
-  if (error) throw error;
+export function Lobby({ user, lobby }: Props) {
   return <>
     {lobby && <h2>Your lobby: {lobby.id}</h2>}
   </>;
--- client/src/pages/GamePage.tsx
+++ client/src/pages/GamePage.tsx
@@ -3,13 +3,29 @@ import { AnonymousLogin } from "../components/AnonymousLogin";
 import { CenteredLayout } from "../components/layout/CenteredLayout";
 import { Lobby } from "../components/Lobby";
 import { User } from "firebase/auth";
+import { GameLobby } from "../model/types";
+import { findOrCreateLobby } from "../model/lobby-api";
 
 export function GamePage() {
   const [user, setUser] = useState<User | null>(null);
+  const [lobby, setLobby] = useState<GameLobby | null>(null);
+  const [error, setError] = useState<any>(null);
+  if (error) throw error;
+
+  async function handleLogin(newUser: User) {
+    setUser(newUser);
+    try {
+      const newLobby = await findOrCreateLobby(newUser);
+      setLobby(newLobby);
+    } catch (e: any) {
+      setError(e);
+    }
+  }
+
   return <CenteredLayout>
-    {user ? <Lobby user={user} /> : (<>
+    {user && lobby ? <Lobby user={user} lobby={lobby} /> : (<>
       <h1 style={{ marginBottom: "1em" }}>Cards Against Animals</h1>
-      <AnonymousLogin onLogin={(user) => setUser(user)} />
+      <AnonymousLogin onLogin={handleLogin} />
     </>)}
   </CenteredLayout>;
 }
