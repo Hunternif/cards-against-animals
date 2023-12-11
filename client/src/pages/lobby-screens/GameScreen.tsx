85ad04f58a95e4bda68b1369690eea8b152ccc57
Hunternif <hunternif@gmail.com>
2023-12-11 22:03:41 +0000

    Response cards can be selected

--- client/src/pages/lobby-screens/GameScreen.tsx
+++ client/src/pages/lobby-screens/GameScreen.tsx
@@ -3,7 +3,7 @@ import { CenteredLayout } from "../../components/layout/CenteredLayout";
 import { CardInGame, DeckCard, GameLobby, PromptCardInGame, ResponseCardInGame } from "../../shared/types";
 import { FillLayout } from "../../components/layout/FillLayout";
 import { RowLayout } from "../../components/layout/RowLayout";
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 
 interface ScreenProps {
   lobby: GameLobby,
@@ -13,10 +13,8 @@ interface ScreenProps {
 interface CardProps {
   card: CardInGame,
   selectable?: boolean,
-}
-
-const screenStyle: CSSProperties = {
-
+  selected?: boolean,
+  onToggle?: (selected: boolean) => void,
 }
 
 const containerStyle: CSSProperties = {
@@ -42,14 +40,18 @@ const botRowStyle: CSSProperties = {
   justifyContent: "center",
 }
 
-function Card({ card, selectable }: CardProps) {
+function Card({ card, selectable, selected, onToggle }: CardProps) {
   const isPrompt = card instanceof PromptCardInGame;
   const isResponse = card instanceof ResponseCardInGame;
   const promptStyle = isPrompt ? "card-prompt" : "";
   const responseStyle = isResponse ? "card-response" : "";
   const selectableStyle = selectable ? "hoverable-card" : "";
-  const className = `game-card ${promptStyle} ${responseStyle} ${selectableStyle}`;
-  return <div className={className}>
+  const selectedStyle = selected ? "selected" : "";
+  const className = `game-card ${promptStyle} ${responseStyle} ${selectableStyle} ${selectedStyle}`;
+  function handleClick() {
+    if (onToggle) onToggle(!selected);
+  }
+  return <div className={className} onClick={handleClick}>
     <span>{card.content}</span>
   </div>;
 }
@@ -61,15 +63,31 @@ const hand = new Array<ResponseCardInGame>(10).fill(dummyResponse, 0, 10);
 
 export function GameScreen({ lobby, user }: ScreenProps) {
   const [handCards] = [hand];
+  // Set of card ids:
+  const [selectedCards, setSelectedCards] = useState<Set<string>>(new Set());
+  function selectCard(cardID: string) {
+    setSelectedCards(new Set(selectedCards).add(cardID));
+  }
+  function deselectCard(cardID: string) {
+    const newSelection = new Set(selectedCards);
+    newSelection.delete(cardID);
+    setSelectedCards(newSelection);
+  }
   return (
-    <CenteredLayout className="game-screen" style={screenStyle}>
+    <CenteredLayout className="game-screen">
       <div style={containerStyle}>
         <div className="game-top-row" style={{ ...rowStyle, ...topRowStyle }}>
           <Card card={dummyPrompt} />
         </div>
         <div className="game-bottom-row" style={{ ...rowStyle, ...botRowStyle }}>
           {handCards.map((card, i) =>
-            <Card key={i} card={card} selectable={true} />
+            <Card key={i} card={card}
+              selectable={true}
+              selected={selectedCards.has(card.id)}
+              onToggle={(selected) => {
+                if (selected) selectCard(card.id);
+                else deselectCard(card.id);
+              }} />
           )}
         </div>
       </div>
