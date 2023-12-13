e9f42c548edf6a0dbdfc27d8bc924c2e1aed6ca8
Hunternif <hunternif@gmail.com>
2023-12-13 23:54:55 +0000

    Render svg icon using text color in GameButton

--- client/src/components/LobbyCreatorControls.tsx
+++ client/src/components/LobbyCreatorControls.tsx
@@ -1,10 +1,9 @@
 import { CSSProperties, useState } from "react";
 import { GameLobby } from "../shared/types";
 import { DeckSelector } from "./DeckSelector";
-import play_button from '../assets/play_button.svg'
 import { startLobby } from "../model/lobby-api";
 import { LoadingSpinner } from "./utils";
-import { GameButton } from "./Buttons";
+import { GameButton, IconPlay } from "./Buttons";
 
 interface Props {
   lobby: GameLobby,
@@ -45,7 +44,7 @@ export function LobbyCreatorControls({ lobby }: Props) {
       <GameButton accent style={startButtonStyle} className="start-button"
         onClick={handleStart}
         disabled={lobby.deck_ids.size == 0}
-        icon={play_button}>
+        icon={<IconPlay/>}>
         Start
       </GameButton>
     </div>
