405db2cfa04beadaddb6ff749a4f4334ad19130f
Hunternif <hunternif@gmail.com>
2023-12-17 22:42:55 +0000

    Add menu option to end lobby

--- client/src/components/GameMenu.tsx
+++ client/src/components/GameMenu.tsx
@@ -1,30 +1,41 @@
 import { Dropdown } from "react-bootstrap";
 import { IconHamburger } from "./Icons";
-import { CSSProperties } from "react";
+import { CSSProperties, useContext } from "react";
 import { CustomDropdown } from "./CustomDropdown";
-import { leaveLobby } from "../model/lobby-api";
+import { endLobby, leaveLobby } from "../model/lobby-api";
 import { User } from "firebase/auth";
-import { GameLobby } from "../shared/types";
+import { GameLobby, GameTurn } from "../shared/types";
 import { useNavigate } from "react-router-dom";
+import { ErrorContext } from "./ErrorContext";
 
 interface MenuProps {
   lobby: GameLobby,
   user: User,
+  turn: GameTurn,
   className?: string,
   style?: CSSProperties,
 }
 
-export function GameMenu({ lobby, user, className, style }: MenuProps) {
+export function GameMenu({ lobby, turn, user, className, style }: MenuProps) {
   const navigate = useNavigate();
+  const { setError } = useContext(ErrorContext);
+  const isJudge = turn.judge_uid === user.uid;
+
   async function handleLeave() {
     await leaveLobby(lobby, user);
     navigate("/");
   }
+
+  async function handleEnd() {
+    await endLobby(lobby).catch((e) => setError(e));
+  }
+
   return (
     <CustomDropdown className={className} style={style}
       toggle={<IconHamburger />} toggleClassName="game-menu-icon">
       <Dropdown.Menu>
         <Dropdown.Item onClick={handleLeave}>Leave</Dropdown.Item>
+        {isJudge && <Dropdown.Item onClick={handleEnd}>End lobby</Dropdown.Item>}
       </Dropdown.Menu>
     </CustomDropdown>
   );
