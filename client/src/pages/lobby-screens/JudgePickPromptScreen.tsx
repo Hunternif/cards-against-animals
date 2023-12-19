90d9ed721a6b59ae9961b7c79a0c6cd04b45961c
Hunternif <hunternif@gmail.com>
2023-12-19 21:59:33 +0000

    Offer to end game on prompt picking screen

--- client/src/pages/lobby-screens/JudgePickPromptScreen.tsx
+++ client/src/pages/lobby-screens/JudgePickPromptScreen.tsx
@@ -3,7 +3,7 @@ import { CSSProperties, useContext, useEffect, useState } from "react";
 import { GameButton } from "../../components/Buttons";
 import { PromptCard } from "../../components/Cards";
 import { ErrorContext } from "../../components/ErrorContext";
-import { LoadingSpinner } from "../../components/LoadingSpinner";
+import { Delay, LoadingSpinner } from "../../components/LoadingSpinner";
 import { CenteredLayout } from "../../components/layout/CenteredLayout";
 import { endLobby } from "../../model/lobby-api";
 import { discardPrompt, getPromptCount, pickNewPrompt, playPrompt } from "../../model/turn-api";
@@ -29,16 +29,20 @@ const midRowStyle: CSSProperties = {
 }
 
 const botRowStyle: CSSProperties = {
+  position: "relative",
   marginTop: "1.5rem",
   height: "3rem",
   display: "flex",
-  justifyContent: "center",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "stretch",
+  gap: "1rem",
 }
 
 /** Aligned to the right of the centered card */
 const sideSectionStyle: CSSProperties = {
   position: "absolute",
-  margin: "2rem",
+  marginLeft: "2rem",
   left: "100%",
 }
 
@@ -53,6 +57,7 @@ export function JudgePickPromptScreen({ lobby, turn }: TurnProps) {
   const [cardCount, setCardCount] = useState(-1);
   const [initialLoaded, setInitialLoaded] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [ending, setEnding] = useState(false);
   const { setError } = useContext(ErrorContext);
 
   async function getInitialPrompt() {
@@ -89,6 +94,14 @@ export function JudgePickPromptScreen({ lobby, turn }: TurnProps) {
     }
   }
 
+  async function handleEndGame() {
+    setEnding(true);
+    await endLobby(lobby).catch((e) => {
+      setError(e);
+      setEnding(false);
+    });
+  }
+
   useEffect(() => {
     getInitialPrompt().catch((e) => setError(e));
   }, [lobby]);
@@ -109,48 +122,49 @@ export function JudgePickPromptScreen({ lobby, turn }: TurnProps) {
     <div style={containerStyle}>
       {!initialLoaded ? (
         <LoadingSpinner delay text="Loading deck..." />
-      ) : prompt ? (<>
+      ) : <>
         <div style={midRowStyle}>
-          <PromptCard card={prompt} />
-          <div style={sideSectionStyle}>
-            <GameButton secondary small onClick={handleChange}>
-              Change
-            </GameButton>
-            <span style={countStyle} className="extra-dim">
-              {cardCount} cards left
-            </span>
-          </div>
+          {prompt ? (
+            <>
+              <PromptCard card={prompt} />
+              <div style={sideSectionStyle}>
+                <GameButton secondary small onClick={handleChange}>
+                  Change
+                </GameButton>
+                <span style={countStyle} className="extra-dim">
+                  {cardCount} cards left
+                </span>
+              </div>
+            </>
+          ) : (
+            <>
+              {cardCount > 0 ? `Oops! ${cardCount} cards remaining, but couldn't load any` :
+                cardCount == 0 ? "No more cards left :(" : ""}
+            </>
+          )}
         </div>
         <div style={botRowStyle}>
-          <GameButton accent onClick={handleSubmit}
-            disabled={!prompt || submitted}>Play</GameButton>
+          {prompt ? (
+            <>
+              <GameButton accent onClick={handleSubmit}
+                disabled={!prompt || submitted}>Play</GameButton>
+              <Delay delayMs={1000}>
+                <GameButton secondary onClick={handleEndGame}
+                  disabled={ending}>
+                  End game
+                </GameButton>
+              </Delay>
+            </>
+          ) : (
+            <>
+              {cardCount == 0 &&
+                <GameButton onClick={handleEndGame} disabled={ending}>
+                  End game
+                </GameButton>}
+            </>
+          )}
         </div>
-      </>) : (
-        <CheckDeckCount lobby={lobby} count={cardCount} />
-      )}
+      </>}
     </div>
   </CenteredLayout>;
-}
-
-interface CountProps {
-  lobby: GameLobby,
-  count: number,
-}
-
-function CheckDeckCount({ lobby, count }: CountProps) {
-  const { setError } = useContext(ErrorContext);
-
-  async function handleEndGame() {
-    await endLobby(lobby).catch((e) => setError(e));
-  }
-
-  return <>
-    <div style={midRowStyle}>
-      {count > 0 ? `${count} cards remaining` :
-        count == 0 ? "No more cards left :(" : ""}
-    </div>
-    <div style={botRowStyle}>
-      {count == 0 && <GameButton onClick={handleEndGame}>End game</GameButton>}
-    </div>
-  </>
 }
