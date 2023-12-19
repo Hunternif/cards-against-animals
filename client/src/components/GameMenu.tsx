9ba07fd485657c7b0b5de0eab63dec5f4b7a9351
Hunternif <hunternif@gmail.com>
2023-12-19 23:27:32 +0000

    GameMenu displays player name and spectator status

--- client/src/components/GameMenu.tsx
+++ client/src/components/GameMenu.tsx
@@ -1,25 +1,37 @@
-import { Dropdown } from "react-bootstrap";
-import { IconHamburger } from "./Icons";
-import { CSSProperties, useContext } from "react";
-import { CustomDropdown } from "./CustomDropdown";
-import { endLobby, leaveLobby } from "../model/lobby-api";
 import { User } from "firebase/auth";
-import { GameLobby, GameTurn } from "../shared/types";
+import { CSSProperties, useContext } from "react";
+import { Dropdown } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { endLobby, leaveLobby } from "../model/lobby-api";
+import { GameLobby, GameTurn, PlayerInLobby } from "../shared/types";
+import { CustomDropdown } from "./CustomDropdown";
 import { ErrorContext } from "./ErrorContext";
 
 interface MenuProps {
   lobby: GameLobby,
   user: User,
   turn: GameTurn,
+  players: PlayerInLobby[],
   className?: string,
   style?: CSSProperties,
 }
 
-export function GameMenu({ lobby, turn, user, className, style }: MenuProps) {
+const rowStyle: CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  gap: "1rem",
+  zIndex: 99,
+}
+
+export function GameMenu(
+  { lobby, turn, user, players, className, style }: MenuProps
+) {
   const navigate = useNavigate();
   const { setError } = useContext(ErrorContext);
   const isJudge = turn.judge_uid === user.uid;
+  const isSpectator = players.find((p) => p.uid === user.uid)?.role === "spectator";
 
   async function handleLeave() {
     await leaveLobby(lobby, user)
@@ -32,12 +44,18 @@ export function GameMenu({ lobby, turn, user, className, style }: MenuProps) {
   }
 
   return (
-    <CustomDropdown className={className} style={style}
-      toggle={<IconHamburger />} toggleClassName="game-menu-icon">
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={handleLeave}>Leave</Dropdown.Item>
-        {isJudge && <Dropdown.Item onClick={handleEnd}>End game</Dropdown.Item>}
-      </Dropdown.Menu>
-    </CustomDropdown>
+    <div style={{ ...rowStyle, ...style }}>
+      <CustomDropdown className={className} showArrow
+        toggle={
+          <span className="light">
+            {user.displayName}{isSpectator && " (spectator)"}
+          </span>
+        } toggleClassName="game-menu-icon">
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={handleLeave}>Leave</Dropdown.Item>
+          {isJudge && <Dropdown.Item onClick={handleEnd}>End game</Dropdown.Item>}
+        </Dropdown.Menu>
+      </CustomDropdown>
+    </div>
   );
 }
