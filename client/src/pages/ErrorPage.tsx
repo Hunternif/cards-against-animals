3be11b014f48370eeb70f5ec986c3f6375b6a1c8
Hunternif <hunternif@gmail.com>
2023-12-19 22:28:24 +0000

    Add button to "Go home" on lobby-end page and error page

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,7 +1,10 @@
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router-dom";
 import { CenteredLayout } from "../components/layout/CenteredLayout";
-import { ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 import confused_kitty from '../assets/confused_kitty.jpg'
+import { GameButton } from "../components/Buttons";
+
+
 
 function getErrorMessage(error: any): ReactNode {
   if (error instanceof Error) {
@@ -15,8 +18,19 @@ function getErrorMessage(error: any): ReactNode {
   }
 }
 
+const botRowStyle: CSSProperties = {
+  position: "relative",
+  marginTop: "1.5rem",
+  height: "3rem",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "center",
+}
+
 export function ErrorPage() {
   const error = useRouteError();
+  const navigate = useNavigate();
   return <CenteredLayout style={{ maxWidth: "500px" }}>
     <h1>Oops!</h1>
     <p>Sorry, an unexpected error has occurred.</p>
@@ -24,5 +38,8 @@ export function ErrorPage() {
       <i>{getErrorMessage(error)}</i>
     </p>}
     <img src={confused_kitty} style={{ width: "100%" }} />
+    <div style={botRowStyle}>
+      <GameButton secondary onClick={() => navigate("/")}>Go home</GameButton>
+    </div>
   </CenteredLayout>
 }
