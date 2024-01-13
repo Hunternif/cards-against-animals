9a6e9d840a7567d6c4e3df8d3b32d22444efb6d4
Hunternif <hunternif@gmail.com>
2024-01-13 19:34:46 +0000

    Styling for lobby settings & 'select' component

--- client/src/components/LobbySettings.tsx
+++ client/src/components/LobbySettings.tsx
@@ -1,5 +1,4 @@
 import { CSSProperties } from "react";
-import { Col, Dropdown, Form, FormGroup, Row } from "react-bootstrap";
 import { GameLobby } from "../shared/types";
 
 interface Props {
@@ -10,25 +9,34 @@ interface Props {
 const formStyle: CSSProperties = {
   width: "100%",
   padding: "1em 2em",
+  // Display style is controlled in CSS with media query
+  // display: "flex",
+  // flexDirection: "column",
+  gap: "0.25em",
 }
 
 export function LobbySettings({ lobby, readOnly }: Props) {
   return (
-    <Form style={formStyle}>
-      <FormGroup as={Row}>
-        <Form.Label column xs="6">Play until</Form.Label>
-        <Col xs="6">
-          <Dropdown>
-            <Dropdown.Toggle variant="secondary" className="light-button"
-              disabled={readOnly}>
-              Forever
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item>X turns</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Col>
-      </FormGroup>
-    </Form>
+    <div style={formStyle} className="lobby-settings-container">
+      <FormItem />
+      <FormItem />
+      <FormItem />
+    </div>
   );
+}
+
+function FormItem() {
+  return <div style={{
+    // flex: "1 1 auto",
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gap: "1em",
+    alignItems: "baseline",
+  }}>
+    <span style={{ textAlign: "end" }}>Play until</span>
+    <select style={{ maxWidth: "12em" }} className="">
+      <option value="forever">Forever</option>
+      <option value="x_turns">X Turns</option>
+    </select>
+  </div>;
 }
