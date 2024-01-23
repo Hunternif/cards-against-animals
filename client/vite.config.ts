a9bf3781c135003d5decd57ecbce2f71bf8c2649
Hunternif <hunternif@gmail.com>
2024-01-23 05:00:05 +0000

    Add html metadata for Discord preview, via text-replacing build plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { Plugin, defineConfig } from 'vite';
+import firebaseConfig from '../firebase-config.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    transformHtmlPlugin({ webUrl: firebaseConfig.webUrl }),
+  ],
+  // Keep file names without hashes.
+  // Thanks to https://www.fabiofranchino.com/log/how-to-remove-hashing-in-vite-built-file-names/
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
+
+/**
+ * From https://stackoverflow.com/a/75644929/1093712
+ * Given input { key: value }, replaces string $key$ with value in html.
+ */
+function transformHtmlPlugin(data: any): Plugin {
+  return {
+    name: 'transform-html',
+    transformIndexHtml: {
+      enforce: 'pre',
+      transform(this, html) {
+        return html.replace(
+          /\$(\w+)\$/gi,
+          (match, p1) => data[p1] || ''
+        );
+      }
+    }
+  }
+};
