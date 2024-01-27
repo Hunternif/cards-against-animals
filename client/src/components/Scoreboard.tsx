5c9f2d0897735cc9947313dd86c73e612990e0c5
Hunternif <hunternif@gmail.com>
2024-01-27 19:29:03 +0000

    Show 0, 1 stars on scoreboard

--- client/src/components/Scoreboard.tsx
+++ client/src/components/Scoreboard.tsx
@@ -28,16 +28,14 @@ export function Scoreboard({ lobby, players }: Props) {
         <tbody>
           {playersByScore.map((player) => <tr key={player.uid}>
             <td className="sb-col-score">
-              {player.score > 0 && <>
-                <IconStarInline />{player.score > 1 && ` ${player.score}`}
-              </>}
+              <IconStarInline /> {player.score}
               {/* {"⭐".repeat(score)} */}
             </td>
             <td className="sb-col-name">{player.name}</td>
             {showLikes && (
               <td className="sb-col-score">
                 {player.likes > 0 && <>
-                  <IconHeartInline />{player.likes > 1 && ` ${player.likes}`}
+                  <IconHeartInline /> {player.likes}
                 </>}
               </td>
             )}
