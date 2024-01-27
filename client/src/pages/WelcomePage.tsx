f4d70ddf3784a5139b35cc9240df18f277d08d72
Hunternif <hunternif@gmail.com>
2024-01-27 21:46:02 +0000

    Display errors on welcome page

--- client/src/pages/WelcomePage.tsx
+++ client/src/pages/WelcomePage.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { ErrorContext } from "../components/ErrorContext";
+import { ErrorModal } from "../components/ErrorModal";
 import { LoginScreen } from "./lobby-screens/LoginScreen";
 
 export function WelcomePage() {
   // This could be common layout shared between all game screens
-  return <LoginScreen />;
+  const [error, setError] = useState(null);
+  return <>
+    <ErrorModal error={error} setError={setError} />
+    <ErrorContext.Provider value={{ error, setError }}>
+      <LoginScreen />
+    </ErrorContext.Provider>
+  </>;
 }
