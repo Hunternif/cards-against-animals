150c02d5f008d559cf3185ba32484e1e4c349fce
Hunternif <hunternif@gmail.com>
2024-03-03 03:16:35 +0000

    Mark cards as editable in admin deck view

--- client/src/pages/admin-screens/admin-components/AdminDeck.tsx
+++ client/src/pages/admin-screens/admin-components/AdminDeck.tsx
@@ -3,7 +3,7 @@ import { ErrorContext } from "../../../components/ErrorContext";
 import { LoadingSpinner } from "../../../components/LoadingSpinner";
 import { useEffectOnce } from "../../../components/utils";
 import { loadDeck } from "../../../model/deck-api";
-import { Deck } from "../../../shared/types";
+import { Deck, DeckCard, PromptDeckCard } from "../../../shared/types";
 
 interface Props {
   deckID: string,
@@ -15,6 +15,20 @@ interface Props {
 export function AdminDeck({ deckID }: Props) {
   const [deck, setDeck] = useState<Deck | null>(null);
   const { setError } = useContext(ErrorContext);
+  const [editedCards, setEditedCards] = useState<DeckCard[]>([]);
+
+  function isEdited(card: DeckCard): boolean {
+    return editedCards.findIndex((c) => c.id === card.id) > -1;
+  }
+  function startEditCard(card: DeckCard) {
+    if (!isEdited(card)) {
+      const editListCopy = editedCards.slice();
+      editListCopy.push(card);
+      setEditedCards(editListCopy);
+    }
+  }
+
+  // Load decks
   useEffectOnce(() => {
     if (!deck) {
       loadDeck(deckID).then((val) => setDeck(val))
@@ -27,23 +41,39 @@ export function AdminDeck({ deckID }: Props) {
     <table className="admin-deck">
       <tbody>
         {deck.prompts.map((card) => (
-          <tr key={card.id} className="row-prompt">
-            <td className="col-card-id">{card.id}</td>
-            <td className="col-card-content">
-              <span className="content">{card.content}</span>
-              <div className="prompt-pick-number">{card.pick}</div>
-            </td>
-            <td className="col-card-tags">{card.tags.join(", ")}</td>
-          </tr>
+          <CardRow key={card.id} card={card}
+            edited={isEdited(card)}
+            onClick={() => startEditCard(card)}
+          />
         ))}
         {deck.responses.map((card) => (
-          <tr key={card.id} className="row-response">
-            <td className="col-card-id">{card.id}</td>
-            <td className="col-card-content">{card.content}</td>
-            <td className="col-card-tags">{card.tags.join(", ")}</td>
-          </tr>
+          <CardRow key={card.id} card={card}
+            edited={isEdited(card)}
+            onClick={() => startEditCard(card)}
+          />
         ))}
       </tbody>
     </table>
   </>;
+}
+
+interface RowProps {
+  card: DeckCard,
+  edited?: boolean,
+  onClick?: () => void,
+}
+
+function CardRow({ card, edited, onClick }: RowProps) {
+  const editableClass = edited ? "edited" : "editable";
+  const isPrompt = card instanceof PromptDeckCard;
+  const cardClass = isPrompt ? "row-prompt" : "row-response";
+  return <tr className={`card-row ${cardClass} ${editableClass}`}
+    onClick={onClick}>
+    <td className="col-card-id">{card.id}</td>
+    <td className="col-card-content">
+      <span className="content">{card.content}</span>
+      {isPrompt && <div className="prompt-pick-number">{card.pick}</div>}
+    </td>
+    <td className="col-card-tags">{card.tags.join(", ")}</td>
+  </tr>
 }
