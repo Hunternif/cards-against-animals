c5763bbfd381b507d94c0eac3710e35662550043
Hunternif <hunternif@gmail.com>
2024-05-30 00:16:43 +0100

    Refactor card prompt-pick

--- client/src/pages/lobby-screens/game-components/CardPrompt.tsx
+++ client/src/pages/lobby-screens/game-components/CardPrompt.tsx
@@ -1,9 +1,6 @@
 import { CSSProperties, useContext } from "react";
 import { usePromptVotes, votePrompt } from "../../../model/turn-api";
-import {
-  PromptCardInGame,
-  VoteChoice
-} from "../../../shared/types";
+import { PromptCardInGame, VoteChoice } from "../../../shared/types";
 import { copyFields2 } from "../../../shared/utils";
 import { Downvote, Upvote } from "./CardVotes";
 import { ErrorContext } from "../../../components/ErrorContext";
@@ -14,18 +11,18 @@ import {
   CardBottomRight,
   CardCenterIcon,
   CardContent,
-  LargeCard
+  LargeCard,
 } from "./LargeCard";
 import { Twemoji } from "../../../components/Twemoji";
 import { PlayerAvatar } from "../../../components/PlayerAvatar";
 
 interface PromptCardProps {
   /** Undefined while the judge hasn't picked a prompt yet */
-  card: PromptCardInGame | undefined | null,
-  canVote?: boolean,
-  canSelect?: boolean,
-  selected?: boolean,
-  onClick?: () => void,
+  card: PromptCardInGame | undefined | null;
+  canVote?: boolean;
+  canSelect?: boolean;
+  selected?: boolean;
+  onClick?: () => void;
 }
 
 /** Formats gaps to be longer. */
@@ -43,25 +40,33 @@ export function CardPrompt(props: PromptCardProps) {
 }
 
 interface KnownPromptCardProps extends PromptCardProps {
-  card: PromptCardInGame,
+  card: PromptCardInGame;
 }
 
 /** Prompt that exists (has been chosen and loaded). */
 function KnownPrompt({
-  card, canVote, canSelect, selected, onClick,
+  card,
+  canVote,
+  canSelect,
+  selected,
+  onClick,
 }: KnownPromptCardProps) {
   const { lobby, turn, player } = useGameContext();
   const { setError } = useContext(ErrorContext);
   const [promptVotes] = usePromptVotes(lobby, turn, card);
   const currentVote = promptVotes?.find((v) => v.player_uid === player.uid);
-  const voteClass = currentVote ?
-    (currentVote.choice === "yes" ? "upvoted" : "downvoted") : "";
+  const voteClass = currentVote
+    ? currentVote.choice === "yes"
+      ? "upvoted"
+      : "downvoted"
+    : "";
   const canSelectClass = canSelect ? "hoverable-card" : "";
   const selectedClass = selected ? "selected" : "unselected";
 
   async function vote(choice?: VoteChoice) {
-    await votePrompt(lobby, turn, card, player, choice)
-      .catch((e) => setError(e));
+    await votePrompt(lobby, turn, card, player, choice).catch((e) =>
+      setError(e)
+    );
   }
 
   async function handleUpvote() {
@@ -81,8 +86,10 @@ function KnownPrompt({
     }
   }
   return (
-    <LargeCard className={`card-prompt ${voteClass} ${canSelectClass} ${selectedClass}`}
-      onClick={onClick}>
+    <LargeCard
+      className={`card-prompt ${voteClass} ${canSelectClass} ${selectedClass}`}
+      onClick={onClick}
+    >
       <CardContent>{formatPrompt(card.content)}</CardContent>
       {(canVote || card.pick > 1) && (
         <CardBottom>
@@ -93,9 +100,8 @@ function KnownPrompt({
             </CardBottomLeft>
           )}
           {card.pick > 1 && (
-            <CardBottomRight className="prompt-pick">
-              PICK
-              <div className="prompt-pick-number">{card.pick}</div>
+            <CardBottomRight>
+              <PromptPick pick={card.pick} />
             </CardBottomRight>
           )}
         </CardBottom>
@@ -104,35 +110,44 @@ function KnownPrompt({
   );
 }
 
+export function PromptPick({ pick }: { pick: number }) {
+  return (
+    <div className="prompt-pick">
+      PICK
+      <div className="prompt-pick-number">{pick}</div>
+    </div>
+  );
+}
+
 /** Prompt that hasn't been chosen or hasn't loaded, shows a big "?". */
 export function UnknownPrompt() {
   return (
     <LargeCard className="card-prompt">
-      <CardCenterIcon className="prompt-unknown-icon">
-        ?
-      </CardCenterIcon>
+      <CardCenterIcon className="prompt-unknown-icon">?</CardCenterIcon>
     </LargeCard>
   );
 }
 
 const containerStyle: CSSProperties = {
   height: "auto",
-}
+};
 
 /** Displays the prompt card and the current judge name below it. */
 export function CardPromptWithCzar(props: PromptCardProps) {
   const { judge, isJudge } = useGameContext();
-  return <div className="game-card-placeholder" style={containerStyle}>
-    <CardPrompt {...props} />
-    {(judge && !isJudge) &&
-      <div className="prompt-czar-name"
-        title={`${judge.name} is card czar, will select the best answer.`}>
-        <PlayerAvatar player={judge} />
-        <span className="player-name">
-          {judge.name}
-        </span>
-        <Twemoji className="icon-czar">👑</Twemoji>
-      </div>
-    }
-  </div>;
+  return (
+    <div className="game-card-placeholder" style={containerStyle}>
+      <CardPrompt {...props} />
+      {judge && !isJudge && (
+        <div
+          className="prompt-czar-name"
+          title={`${judge.name} is card czar, will select the best answer.`}
+        >
+          <PlayerAvatar player={judge} />
+          <span className="player-name">{judge.name}</span>
+          <Twemoji className="icon-czar">👑</Twemoji>
+        </div>
+      )}
+    </div>
+  );
 }
