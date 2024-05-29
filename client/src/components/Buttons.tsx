dadbbdc97c62ca2683798d905436daa83d5f72b3
Hunternif <hunternif@gmail.com>
2024-05-29 23:56:27 +0100

    Refactor controls to reuse button styles

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -1,41 +1,33 @@
 import { ReactNode } from "react";
-import { copyFields } from "../shared/utils";
+import {
+  ControlProps,
+  getControlStyle,
+  stripControlProps,
+} from "./FormControls";
 
 interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  iconLeft?: ReactNode,
-  iconRight?: ReactNode,
-  accent?: boolean,
-  secondary?: boolean,
-  light?: boolean,
-  small?: boolean,
-  inline?: boolean,
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ControlProps {
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
 }
 
-export function GameButton(props: ButtonProps) {
-  const accentClass = props.accent ? "accent-button " : "";
-  const lightClass = props.light ? "light-button " : "";
-  const secondaryClass = props.secondary ? "secondary-button " : "";
-  const smallClass = props.small ? "small-button " : "";
-  const inlineClass = props.inline ? "inline-game-button" : "";
-  const className = `game-button ${lightClass}${secondaryClass}${accentClass}${smallClass}${inlineClass}${props.className ?? ""}`;
-  // Remove new fields when passing props to DOM:
-  const propsCopy = copyFields(props, ['accent', 'iconLeft', 'iconRight', 'secondary', 'small', 'light', 'inline']);
-  return <button {...propsCopy}
-    className={className}
-  >
-    {props.iconLeft && <span style={{ display: "flex" }}>
-      {props.iconLeft}
-    </span>}
-    <span style={{ flexGrow: 1 }}>{props.children}</span>
-    {props.iconRight && <span style={{ display: "flex" }}>
-      {props.iconRight}
-    </span>}
-  </button>;
+export function GameButton({ iconLeft, iconRight, ...props }: ButtonProps) {
+  const controlClass = getControlStyle(props);
+  const buttonProps = stripControlProps(props);
+  return (
+    <button {...buttonProps} className={`game-button ${controlClass}`}>
+      {iconLeft && <span style={{ display: "flex" }}>{iconLeft}</span>}
+      <span style={{ flexGrow: 1 }}>{props.children}</span>
+      {iconRight && <span style={{ display: "flex" }}>{iconRight}</span>}
+    </button>
+  );
 }
 
 export function InlineButton(props: React.HTMLProps<HTMLSpanElement>) {
-  return <div className="inline-button-block">
-    <span {...props} className={`inline-button ${props.className ?? ""}`} />
-  </div>;
+  return (
+    <div className="inline-button-block">
+      <span {...props} className={`inline-button ${props.className ?? ""}`} />
+    </div>
+  );
 }
--- client/src/components/FormControls.tsx
+++ client/src/components/FormControls.tsx
@@ -2,21 +2,36 @@ import { ChangeEvent, useContext } from "react";
 import { Checkbox } from "./Checkbox";
 import { ErrorContext } from "./ErrorContext";
 
+export interface ControlProps {
+  className?: string;
+  accent?: boolean;
+  secondary?: boolean;
+  light?: boolean;
+  small?: boolean;
+  inline?: boolean;
+}
 
-interface NumberInputProps {
-  min: number,
-  max: number,
-  step?: number,
-  value: number,
-  disabled?: boolean,
-  onChange: (newValue: number) => Promise<void>,
+interface NumberInputProps extends ControlProps {
+  min: number;
+  max: number;
+  step?: number;
+  value: number;
+  disabled?: boolean;
+  onChange: (newValue: number) => Promise<void>;
 }
 
 /** Form input: integer or float numbers */
-export function NumberInput(
-  { min, max, step, value, disabled, onChange }: NumberInputProps
-) {
+export function NumberInput({
+  min,
+  max,
+  step,
+  value,
+  disabled,
+  onChange,
+  ...props
+}: NumberInputProps) {
   const isInt = step != undefined && Math.floor(step) === step;
+  const controlClass = getControlStyle(props);
   const { setError } = useContext(ErrorContext);
 
   async function handleChange(event: ChangeEvent<HTMLInputElement>) {
@@ -26,51 +41,71 @@ export function NumberInput(
   }
 
   return (
-    <input className="control" disabled={disabled}
-      type="number" min={min} max={max} step={step ?? 1}
-      value={value} onChange={handleChange} />
+    <input
+      className={`control ${controlClass}`}
+      disabled={disabled}
+      type="number"
+      min={min}
+      max={max}
+      step={step ?? 1}
+      value={value}
+      onChange={handleChange}
+    />
   );
 }
 
-
-interface SelectInputProps<T extends string> {
-  value: T,
-  options: SelectOption<T>[],
-  disabled?: boolean,
-  onChange: (newValue: T) => Promise<void>,
+interface SelectInputProps<T extends string> extends ControlProps {
+  value: T;
+  options: SelectOption<T>[];
+  disabled?: boolean;
+  onChange: (newValue: T) => void | Promise<void>;
 }
 type SelectOption<T> = [value: T, label: string];
 
 /** Form input: dropdown list of enum type T. */
-export function SelectInput<T extends string>(
-  { value, disabled, options, onChange }: SelectInputProps<T>
-) {
+export function SelectInput<T extends string>({
+  value,
+  disabled,
+  options,
+  onChange,
+  ...props
+}: SelectInputProps<T>) {
   const { setError } = useContext(ErrorContext);
+  const controlClass = getControlStyle(props);
 
   async function handleSelect(event: ChangeEvent<HTMLSelectElement>) {
     const newValue = event.target.value as T;
-    await onChange(newValue).catch((e) => setError(e));
+    try {
+      await onChange(newValue);
+    } catch (e: any) {
+      setError(e);
+    }
   }
 
   return (
-    <select className="control" disabled={disabled}
-      value={value} onChange={handleSelect}>
-      {options.map((op) => <option key={op[0]} value={op[0]}>{op[1]}</option>)}
+    <select
+      className={`control ${controlClass}`}
+      disabled={disabled}
+      value={value}
+      onChange={handleSelect}
+    >
+      {options.map((op) => (
+        <option key={op[0]} value={op[0]}>
+          {op[1]}
+        </option>
+      ))}
     </select>
   );
 }
 
-
 interface ToggleInputProps {
-  value: boolean,
-  disabled?: boolean,
-  onChange: (newValue: boolean) => Promise<void>,
+  value: boolean;
+  disabled?: boolean;
+  onChange: (newValue: boolean) => Promise<void>;
 }
 
 /** Form input: toggle */
-export function ToggleInput(
-  { value, disabled, onChange }: ToggleInputProps
-) {
+export function ToggleInput({ value, disabled, onChange }: ToggleInputProps) {
   const { setError } = useContext(ErrorContext);
 
   async function handleChange(checked: boolean) {
@@ -79,10 +114,36 @@ export function ToggleInput(
 
   // TODO: make a fancier on/off toggle
   return (
-    <Checkbox
-      checked={value}
-      disabled={disabled}
-      onToggle={handleChange}
-    />
+    <Checkbox checked={value} disabled={disabled} onToggle={handleChange} />
   );
+}
+
+export function getControlStyle({
+  className,
+  accent,
+  light,
+  secondary,
+  small,
+  inline,
+}: ControlProps): string {
+  const classes = new Array<string>();
+  if (className) classes.push(className);
+  if (accent) classes.push("accent-control");
+  if (light) classes.push("light-control");
+  if (secondary) classes.push("secondary-control");
+  if (small) classes.push("small-control");
+  if (inline) classes.push("inline-contorl");
+  return classes.join(" ");
+}
+
+export function stripControlProps<T>({
+  className,
+  accent,
+  light,
+  secondary,
+  small,
+  inline,
+  ...props
+}: ControlProps & T) {
+  return props;
 }
