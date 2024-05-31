bb0b70e2edd641476b847c956d23778b6fcfa8b6
Hunternif <hunternif@gmail.com>
2024-06-01 00:43:23 +0100

    Admin: cards renderd in 1 line, toggle select

--- client/src/pages/admin-screens/admin-components/AdminDeck.tsx
+++ client/src/pages/admin-screens/admin-components/AdminDeck.tsx
@@ -1,13 +1,22 @@
-import { useContext, useState } from "react";
+import { ReactNode, useContext, useState } from "react";
 import { ErrorContext } from "../../../components/ErrorContext";
 import { LoadingSpinner } from "../../../components/LoadingSpinner";
+import { Twemoji } from "../../../components/Twemoji";
 import { useEffectOnce } from "../../../components/utils";
-import { loadDeck } from "../../../model/deck-api";
+import { isOnlyEmojis, loadDeck } from "../../../model/deck-api";
 import { Deck, DeckCard, PromptDeckCard } from "../../../shared/types";
-import { CardContent } from "../../lobby-screens/game-components/LargeCard";
 
 interface Props {
-  deckID: string,
+  deckID: string;
+}
+
+/**
+ * Prompts and responses can have the same ID.
+ * This function returns a prefixed ID that is unique in a list containing
+ * both prompts and responses.
+ */
+function typedID(card: DeckCard): string {
+  return card.type + card.id;
 }
 
 /**
@@ -16,67 +25,88 @@ interface Props {
 export function AdminDeck({ deckID }: Props) {
   const [deck, setDeck] = useState<Deck | null>(null);
   const { setError } = useContext(ErrorContext);
-  const [editedCards, setEditedCards] = useState<DeckCard[]>([]);
+  // Maps card id to card
+  const [selectedCards, setSelectedCards] = useState<Map<string, DeckCard>>(
+    new Map()
+  );
 
-  function isEdited(card: DeckCard): boolean {
-    return editedCards.findIndex((c) =>
-      c.id === card.id && c.type === card.type) > -1;
+  function isSelected(card: DeckCard): boolean {
+    return selectedCards.has(typedID(card));
   }
-  function startEditCard(card: DeckCard) {
-    if (!isEdited(card)) {
-      const editListCopy = editedCards.slice();
-      editListCopy.push(card);
-      setEditedCards(editListCopy);
-    }
+  function toggleSelectedCard(card: DeckCard) {
+    const id = typedID(card);
+    const copy = new Map(selectedCards.entries());
+    if (selectedCards.has(id)) copy.delete(id);
+    else copy.set(id, card);
+    setSelectedCards(copy);
   }
 
   // Load decks
   useEffectOnce(() => {
     if (!deck) {
-      loadDeck(deckID).then((val) => setDeck(val))
+      loadDeck(deckID)
+        .then((val) => setDeck(val))
         .catch((e) => setError(e));
     }
   });
   if (!deck) return <LoadingSpinner />;
 
   // TODO: use react-virtualized to speed up rendering of a large table
-  return <>
-    <table className="admin-deck">
-      <tbody>
-        {deck.prompts.map((card) => (
-          <CardRow key={card.id} card={card}
-            edited={isEdited(card)}
-            onClick={() => startEditCard(card)}
-          />
-        ))}
-        {deck.responses.map((card) => (
-          <CardRow key={card.id} card={card}
-            edited={isEdited(card)}
-            onClick={() => startEditCard(card)}
-          />
-        ))}
-      </tbody>
-    </table>
-  </>;
+  return (
+    <>
+      <table className="admin-deck">
+        <tbody>
+          {deck.prompts.map((card) => (
+            <CardRow
+              key={card.id}
+              card={card}
+              selected={isSelected(card)}
+              onClick={() => toggleSelectedCard(card)}
+            />
+          ))}
+          {deck.responses.map((card) => (
+            <CardRow
+              key={card.id}
+              card={card}
+              selected={isSelected(card)}
+              onClick={() => toggleSelectedCard(card)}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 interface RowProps {
-  card: DeckCard,
-  edited?: boolean,
-  onClick?: () => void,
+  card: DeckCard;
+  selected?: boolean;
+  onClick?: () => void;
 }
 
-function CardRow({ card, edited, onClick }: RowProps) {
-  const editableClass = edited ? "edited" : "editable";
+function CardRow({ card, selected, onClick }: RowProps) {
+  const selectedClass = selected ? "selected" : "selectable";
   const isPrompt = card instanceof PromptDeckCard;
   const cardClass = isPrompt ? "row-prompt" : "row-response";
-  return <tr className={`card-row ${cardClass} ${editableClass}`}
-    onClick={onClick}>
-    <td className="col-card-id">{card.id}</td>
-    <td className="col-card-content">
-      <CardContent contentEditable={edited}>{card.content}</CardContent>
-      {isPrompt && <div className="prompt-pick-number">{card.pick}</div>}
-    </td>
-    <td className="col-card-tags">{card.tags.join(", ")}</td>
-  </tr>
+  return (
+    <tr className={`card-row ${cardClass} ${selectedClass}`} onClick={onClick}>
+      <td className="col-card-id">{card.id}</td>
+      <td className="col-card-content">
+        <CardContentRow>{card.content}</CardContentRow>
+        {isPrompt && <div className="prompt-pick-number">{card.pick}</div>}
+      </td>
+      <td className="col-card-tags">{card.tags.join(", ")}</td>
+    </tr>
+  );
+}
+
+interface CardContentRowProps {
+  children: ReactNode;
+}
+function CardContentRow(props: CardContentRowProps) {
+  const content = props.children?.toString() ?? "";
+  const emojiClass = isOnlyEmojis(content) ? "emoji-only " : "";
+  return (
+    <Twemoji {...props} className={`card-content-admin-row ${emojiClass}`} />
+  );
 }
