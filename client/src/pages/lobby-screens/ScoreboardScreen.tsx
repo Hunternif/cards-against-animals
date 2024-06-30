5f0c9b743402c9b2aa7e706fc62ee55ac179c3be
Hunternif <hunternif@gmail.com>
2024-06-30 22:39:05 +0100

    Remove "Go home" button from scoreboard screen

--- client/src/pages/lobby-screens/ScoreboardScreen.tsx
+++ client/src/pages/lobby-screens/ScoreboardScreen.tsx
@@ -1,33 +1,29 @@
-import { User } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { GameButton } from "../../components/Buttons";
-import { Delay } from "../../components/Delay";
-import { Scoreboard } from "../../components/Scoreboard";
-import { FillLayout } from "../../components/layout/FillLayout";
-import { GameLayout } from "../../components/layout/GameLayout";
-import { GameLobby, PlayerInLobby } from "../../shared/types";
+import { User } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { Scoreboard } from '../../components/Scoreboard';
+import { FillLayout } from '../../components/layout/FillLayout';
+import { GameLayout } from '../../components/layout/GameLayout';
+import { GameLobby, PlayerInLobby } from '../../shared/types';
 
 interface Props {
-  lobby: GameLobby,
-  user: User,
-  players: PlayerInLobby[],
+  lobby: GameLobby;
+  user: User;
+  players: PlayerInLobby[];
 }
 
 export function ScoreboardScreen({ lobby, players }: Props) {
   const navigate = useNavigate();
-  return <FillLayout className="scoreboard-screen">
-    <GameLayout>
-      <header>
-        <h2>Scoreboard</h2>
-      </header>
-      <section>
-        <Scoreboard lobby={lobby} players={players} />
-      </section>
-      <footer>
-        <Delay>
-          <GameButton secondary onClick={() => navigate("/")}>Go home</GameButton>
-        </Delay>
-      </footer>
-    </GameLayout>
-  </FillLayout>;
+  return (
+    <FillLayout className="scoreboard-screen">
+      <GameLayout>
+        <header>
+          <h2>Scoreboard</h2>
+        </header>
+        <section>
+          <Scoreboard lobby={lobby} players={players} />
+        </section>
+        <footer></footer>
+      </GameLayout>
+    </FillLayout>
+  );
 }
