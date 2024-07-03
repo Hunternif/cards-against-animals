b480a51966aff5fa0faa95ddf9d0b2013330d0b8
Hunternif <hunternif@gmail.com>
2024-07-03 21:42:30 +0100

    Lobby creator sees the 'go home' button

--- client/src/pages/lobby-screens/ScoreboardScreen.tsx
+++ client/src/pages/lobby-screens/ScoreboardScreen.tsx
@@ -1,5 +1,7 @@
 import { User } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { GameButton } from '../../components/Buttons';
+import { Delay } from '../../components/Delay';
 import { Scoreboard } from '../../components/Scoreboard';
 import { FillLayout } from '../../components/layout/FillLayout';
 import { GameLayout } from '../../components/layout/GameLayout';
@@ -11,7 +13,7 @@ interface Props {
   players: PlayerInLobby[];
 }
 
-export function ScoreboardScreen({ lobby, players }: Props) {
+export function ScoreboardScreen({ lobby, user, players }: Props) {
   const navigate = useNavigate();
   return (
     <FillLayout className="scoreboard-screen">
@@ -22,7 +24,15 @@ export function ScoreboardScreen({ lobby, players }: Props) {
         <section>
           <Scoreboard lobby={lobby} players={players} />
         </section>
-        <footer></footer>
+        <footer>
+          {user.uid === lobby.creator_uid && (
+            <Delay>
+              <GameButton secondary onClick={() => navigate('/')}>
+                Go home
+              </GameButton>
+            </Delay>
+          )}
+        </footer>
       </GameLayout>
     </FillLayout>
   );
