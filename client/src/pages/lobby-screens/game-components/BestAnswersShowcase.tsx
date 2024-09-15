dd051f63c0ea5b3837bf11c80d6e8d383abdf5a2
Hunternif <hunternif@gmail.com>
2024-09-16 00:44:23 +0100

    Optimize number of divs on answer showcase

--- client/src/pages/lobby-screens/game-components/BestAnswersShowcase.tsx
+++ client/src/pages/lobby-screens/game-components/BestAnswersShowcase.tsx
@@ -13,6 +13,16 @@ interface Props {
   lobby: GameLobby;
 }
 
+// TODO: sync this time with answer-showcase.scss
+/** Cards reach the bottom at 0% opacity after this time. */
+const timeToDisappearSec = 20;
+/** A new answer is added after this time. */
+const cardIntervalSec = 3;
+
+// TODO: use actual card size
+const cardSize = { width: '10rem', height: '14rem' };
+const padding = { x: '2rem', y: '2rem' };
+
 /**
  * Rains down best answers from the current lobby.
  */
@@ -23,9 +33,6 @@ export function BestAnswersShowcase({ lobby }: Props) {
 
   const [bestAnswers] = useAsyncData(getBestAnswers(lobby.id));
   const screenSize = useScreenSize();
-  // TODO: use actual card size
-  const cardSize = { width: '10rem', height: '14rem' };
-  const padding = { x: '2rem', y: '2rem' };
 
   // Predictable RNG seed:
   const firstCard = bestAnswers?.at(0)?.response?.cards?.at(0);
@@ -44,19 +51,29 @@ export function BestAnswersShowcase({ lobby }: Props) {
           // loop forever:
           i = 0;
         }
+
         currentlyVisible = [...currentlyVisible, bestAnswers[i]];
         setVisibleAnswers(currentlyVisible);
         i++;
-      }, 3000);
+      }, cardIntervalSec * 1000);
     }
     return () => {
       clearInterval(interval);
     };
   }, [bestAnswers]);
 
+  const maxCardsOnScreen = Math.ceil(timeToDisappearSec / cardIntervalSec);
+
   return (
     <>
       {visibleAnswers.map((answer, i) => {
+        // Always call RNG the same number of times to preserve positions:
+        const randomY = rng.randomFloat();
+
+        // Check if any cards have reached the bottom:
+        if (visibleAnswers.length - i > maxCardsOnScreen) {
+          return null;
+        }
         return (
           <div
             key={`${answer.prompt.id}${answer.response.cards[0].id}_${i}`}
@@ -64,9 +81,7 @@ export function BestAnswersShowcase({ lobby }: Props) {
             style={{
               opacity: 0.5,
               // TODO: space them evenly
-              left: `calc(${padding.x} + (${screenSize.width}px - 10rem - 2*${
-                padding.x
-              }) * ${rng.randomFloat()})`,
+              left: `calc(${padding.x} + (${screenSize.width}px - 10rem - 2*${padding.x}) * ${randomY})`,
             }}
           >
             <CardStack
