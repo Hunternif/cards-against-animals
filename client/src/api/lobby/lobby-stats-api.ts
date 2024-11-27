7ecc7b1e5f54549826a97a448539aadf650db1df
Hunternif <hunternif@gmail.com>
2024-11-27 20:14:01 +0000

    When searching best responses, reduce minimum like count

--- client/src/api/lobby/lobby-stats-api.ts
+++ client/src/api/lobby/lobby-stats-api.ts
@@ -15,21 +15,25 @@ export async function getBestAnswers(
 ): Promise<ResponseWithPrompt[]> {
   const out = new Array<ResponseWithPrompt>();
   const playerCount = await getActivePlayerCount(lobbyID);
-  const minLikeCount = Math.floor(playerCount / 2);
-  const allTurns = await getAllTurns(lobbyID);
-  for (const turn of allTurns) {
-    const responses = await getAllPlayerResponses(
-      lobbyID,
-      turn.id,
-      minLikeCount,
-    );
-    if (responses.length <= 0) continue;
-    const prompt = await getTurnPrompt(lobbyID, turn);
-    if (prompt) {
-      for (const response of responses) {
-        out.push({ prompt, response });
+  let minLikeCount = Math.floor(playerCount / 2);
+  while (out.length === 0 && minLikeCount >= 0) {
+    const allTurns = await getAllTurns(lobbyID);
+    for (const turn of allTurns) {
+      const responses = await getAllPlayerResponses(
+        lobbyID,
+        turn.id,
+        minLikeCount,
+      );
+      if (responses.length <= 0) continue;
+      const prompt = await getTurnPrompt(lobbyID, turn);
+      if (prompt) {
+        for (const response of responses) {
+          out.push({ prompt, response });
+        }
       }
     }
+    // Reduce required like count in case we got no responses:
+    minLikeCount--;
   }
   return out;
 }
