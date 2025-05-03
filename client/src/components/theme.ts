73a7f7c63ce692d4947f45aac04ab91a55926f82
Hunternif <hunternif@gmail.com>
2025-05-03 23:58:03 +0100

    Rough implement spring theme

--- client/src/components/theme.ts
+++ client/src/components/theme.ts
@@ -1,6 +1,6 @@
-export type SeasonTheme = 'normal' | 'halloween' | 'christmas';
+export type SeasonTheme = 'normal' | 'halloween' | 'christmas' | 'spring';
 
-export const SEASON: SeasonTheme = 'christmas';
+export const SEASON: SeasonTheme = 'spring';
 
 export function isSeason(season: SeasonTheme): boolean {
   return season === SEASON;
