ee59abe798dc862e5684139fc3bdd6ce8bc91c9a
Hunternif <hunternif@gmail.com>
2025-10-09 22:18:50 +0100

    Enable halloween theme 2025

--- client/src/components/theme.ts
+++ client/src/components/theme.ts
@@ -1,6 +1,6 @@
 export type SeasonTheme = 'normal' | 'halloween' | 'christmas' | 'spring';
 
-export const SEASON: SeasonTheme = 'spring';
+export const SEASON: SeasonTheme = 'halloween';
 
 export function isSeason(season: SeasonTheme): boolean {
   return season === SEASON;
